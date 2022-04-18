d0378ffb4e605a3945581203e52d41ee5a315e9c
Liav <oneandonlyliav@gmail.com>
2022-04-18 15:44:48 -0400

    updated various project settings files

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,32 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
     }),
   ],
   devServer: {
     open: true,
     historyApiFallback: true,
     hot: true,
-    watchFiles: ["src/**/*.html", "public/**/*"],
+    watchFiles: ['src/**/*.html', 'public/**/*'],
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-  mode: "development",
-  devtool: "source-map",
+  mode: 'development',
+  devtool: 'source-map',
 };
