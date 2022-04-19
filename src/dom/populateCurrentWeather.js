e29800f6531a142f96174f88cf20ea11692b2903
Liav <oneandonlyliav@gmail.com>
2022-04-19 17:01:12 -0400

    added current weather dom module

---
+++ src/dom/populateCurrentWeather.js
@@ -0,0 +1,11 @@
+export default async function populateCurrentWeatherPrimary(cw) {
+  console.log(cw);
+  const currentWeatherTemp = document.getElementById('current-weather-temp');
+  const currentWeatherIcon = document.getElementById('current-weather-icon');
+  const currentFeelsLike = document.getElementById('current-feels-like');
+  const currentHumidity = document.getElementById('current-humidity');
+  currentWeatherTemp.textContent = `${Math.floor(cw.temp)}°F`;
+  currentWeatherIcon.src = `http://openweathermap.org/img/wn/${cw.weather[0].icon}@4x.png`;
+  currentFeelsLike.textContent = `Feels like ${Math.floor(cw.feels_like)}°F`;
+  currentHumidity.textContent = `Humidity: ${Math.floor(cw.humidity)}%`;
+}
