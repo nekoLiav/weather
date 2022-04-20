18fcb03d6cdf9e55d1e263d263c7888f3494534c
Liav <oneandonlyliav@gmail.com>
2022-04-19 21:10:55 -0400

    various minor html/style changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import fetchGeo from './core/fetchGeo';
 import fetchWeather from './core/fetchWeather';
+import geolocation from './core/geolocation';
+import populateLocation from './dom/populateLocation';
+
 import './styles/meyerweb-reset.css';
 import './styles/style.css';
 
-// fetchWeather(40.05888, -83.0603264);
-// fetchGeo(40.05888, -83.0603264);
+fetchWeather(40.05888, -83.0603264);
+populateLocation('Columbus, OH, US');
+geolocation();
 
 const locationForm = document.getElementById('location-form');
 locationForm.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     e.preventDefault();
-    console.log(e.target.value);
+    fetchGeo(e.target.value);
     locationForm.reset();
+    e.target.blur();
   }
 });
