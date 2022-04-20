ccc8b2aaa28172d2f834b8acbc3f5fc372fd1adb
Liav <oneandonlyliav@gmail.com>
2022-04-20 18:33:00 -0400

    removed redundant check

--- src/core/fetchGeo.js
+++ src/core/fetchGeo.js
@@ -1,4 +1,6 @@
 import populateSearch from '../dom/populateSearch';
+import fetchWeather from './fetchWeather';
+import populateLocation from '../dom/populateLocation';
 
 export default async function fetchGeo(formInput) {
   const isUSZIP = /\d{5}/g;
@@ -15,8 +17,7 @@ export default async function fetchGeo(formInput) {
       `https://api.openweathermap.org/geo/1.0/zip?zip=${formInput}&limit=5&appid=961f06db876d364746ee34fafc6c8bba`
     );
     const geoData = await geoResponse.json();
-    if (geoData.length > 1) {
-      populateSearch(geoData);
-    }
+    fetchWeather(geoData.lat, geoData.lon);
+    populateLocation(geoData.name);
   }
 }
