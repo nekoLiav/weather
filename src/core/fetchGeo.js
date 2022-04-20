34c9aa47c66f362409ffbb9f2c12229fe89781bf
Liav <oneandonlyliav@gmail.com>
2022-04-19 21:08:53 -0400

    added regex to delegate US zip lookups

--- src/core/fetchGeo.js
+++ src/core/fetchGeo.js
@@ -1,9 +1,22 @@
-import populateLocation from '../dom/populateLocation';
+import populateSearch from '../dom/populateSearch';
 
-export default async function fetchGeo(lat, lon) {
-  const geoResponse = await fetch(
-    `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=961f06db876d364746ee34fafc6c8bba`
-  );
-  const geoData = await geoResponse.json();
-  populateLocation(geoData[0]);
+export default async function fetchGeo(formInput) {
+  const isUSZIP = /\d{5}/g;
+  if (!isUSZIP.test(formInput)) {
+    const geoResponse = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${formInput}&limit=5&appid=961f06db876d364746ee34fafc6c8bba`
+    );
+    const geoData = await geoResponse.json();
+    if (geoData.length > 1) {
+      populateSearch(geoData);
+    }
+  } else {
+    const geoResponse = await fetch(
+      `http://api.openweathermap.org/geo/1.0/zip?zip=${formInput}&limit=5&appid=961f06db876d364746ee34fafc6c8bba`
+    );
+    const geoData = await geoResponse.json();
+    if (geoData.length > 1) {
+      populateSearch(geoData);
+    }
+  }
 }
